c5dba0d62b0b4609b813dbf15a011ce7dcd4803b
inbarvernia <78118133+inbarvernia@users.noreply.github.com>
2021-05-04 13:58:41 +0100

    Test for deposit method increasing balance by amount specified

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -5,5 +5,9 @@ describe BankAccount do
     it "is a method that takes two arguments" do
       expect(subject).to respond_to(:deposit).with(2).arguments
     end
+
+    it "increases balance by amount specified" do
+      expect {subject.deposit(1000, "10-01-2012")}.to change {subject.balance}.by(1000)
+    end
   end
 end
