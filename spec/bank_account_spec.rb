2f2a69a1de3f1108e0a112257266d07ccaea6e33
inbarvernia <78118133+inbarvernia@users.noreply.github.com>
2021-05-04 14:07:10 +0100

    Test for print_statement method printing a header

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -10,4 +10,10 @@ describe BankAccount do
       expect {subject.deposit(1000, "10-01-2012")}.to change {subject.balance}.by(1000)
     end
   end
+
+  describe '#print_statement' do
+    it "prints a header" do
+      expect {subject.print_statement}.to output("date || credit || debit || balance").to_stdout
+    end
+  end
 end
