7a187547bfd060adefaa7c683f4d2252f382c874
inbarvernia <78118133+inbarvernia@users.noreply.github.com>
2021-05-04 13:46:13 +0100

    Wrote first test: BankAccount instances respond to a deposit method with two arguments

---
+++ spec/bank_account_spec.rb
@@ -0,0 +1,7 @@
+describe BankAccount do
+  describe '#deposit' do
+    it "is a method that takes two arguments" do
+      expect(subject).to respond_to(:deposit).with(2).arguments
+    end
+  end
+end
