6d53cb82b0b520404b25d50e9de5a300ae9dc554
inbarvernia <78118133+inbarvernia@users.noreply.github.com>
2021-05-04 14:37:39 +0100

    Passed test for deposit saving transaction information

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,14 +1,21 @@
 # frozen_string_literal: true
 
 class BankAccount
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
   def initialize
     @balance = 0
+    @transactions = []
   end
 
-  def deposit(amount, _date)
+  def deposit(amount, date)
     @balance += amount
+    transaction = {
+      date: date,
+      amount: amount.to_f,
+      balance: @balance.to_f
+    }
+    @transactions.push(transaction)
   end
 
   def print_statement
