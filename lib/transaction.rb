72770f7c3c2a367942ff34f8a62ee51f917f241d
inbarvernia <78118133+inbarvernia@users.noreply.github.com>
2021-05-05 16:21:00 +0100

    Extracted transaction formatting method to transaction class and tests

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -9,4 +9,15 @@ class Transaction
     @amount = amount.to_f
     @balance = balance.to_f
   end
+
+  def format_for_statement
+    formatted_transaction = "#{@date.strftime('%d/%m/%Y')} || "
+    if @amount.positive?
+      formatted_transaction += "#{format('%.2f', @amount)} || || "
+    else
+      formatted_transaction += "|| #{format('%.2f', @amount.abs)} || "
+      # Using transaction[:amount].abs because it will now appear under debit and should therefore be a positive amount
+    end
+    formatted_transaction += "#{format('%.2f', @balance)}"
+  end
 end
--- spec/units/transaction_spec.rb
+++ spec/units/transaction_spec.rb
@@ -2,14 +2,27 @@ require 'transaction'
 
 describe Transaction do
   let(:date) { Date.new(2012, 1, 10) }
-  # let(:transaction) { Transaction.new(date, 1000, 3000) }
+  let(:transaction) { Transaction.new(date: date, amount: 1000, balance: 3000) }
+  let(:withdrawal) { Transaction.new(date: date, amount: -1000, balance: -1000) }
 
   describe '#initialize' do
     it 'creates a new transaction instance with date, amount, and balance attributes' do
-      transaction = Transaction.new(date: date, amount: 1000, balance: 3000)
       expect(transaction.date).to eq(date)
       expect(transaction.amount).to eq(1000.0)
       expect(transaction.balance).to eq(3000.0)
+    end
+  end
+
+  describe '#format_for_statement' do
+    context 'when deposit (i.e. positive amount)' do
+      it 'formats each transaction into a string with amount in credit column' do
+        expect(transaction.format_for_statement).to eq("10/01/2012 || 1000.00 || || 3000.00")
+      end
+    end
+    context 'when withdrawal (i.e. negative amount)' do
+      it 'formats each transaction into a string with amount in debit column' do
+        expect(withdrawal.format_for_statement).to eq("10/01/2012 || || 1000.00 || -1000.00")
+      end
     end
   end
 end
